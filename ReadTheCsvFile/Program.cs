fa0676908638cb1cd41326efff802936ab0de4af
raj <rajvarma007009@gmail.com>
2018-11-21 11:03:59 +0530

    Read The Csv File
    create and read the file

---
+++ ReadTheCsvFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadTheCsvFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            StringBuilder csvfile = new StringBuilder();
+            csvfile.AppendLine("Name, Age");
+            csvfile.AppendLine("Raj,21");
+            csvfile.AppendLine("Varma,21");
+            string csvpath = "D:\\xyz.csv";
+            File.AppendAllText(csvpath, csvfile.ToString());
+            try
+            {
+                string st = File.ReadAllText("D:\\xyz.csv");
+                Console.WriteLine(st);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("the file is not found");
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
